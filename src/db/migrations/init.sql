b4ff5c8801a14ae766f4b90d6a6e9f08932465fa
Мисилин Дмитрий <d_misilin@bingo-boom.com>
2022-09-15 21:18:07 +0300

    some

--- src/db/migrations/init.sql
+++ src/db/migrations/init.sql
@@ -1,16 +1,16 @@
-CREATE TYPE status AS ENUM ('created', 'closed', 'processing', 'completed');
+CREATE TYPE status AS ENUM ('created', 'approved', 'active', 'completed');
 
 CREATE TABLE request (
-    request_id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    request_id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
     type_id INTEGER NOT NULL,
     activity_id INTEGER NOT NULL,
     title VARCHAR(200) NOT NULL,
     description TEXT NOT NULL,
     country_id INTEGER,
     city_id INTEGER,
-    create_dttm TIMESTAMPTZ DEFAULT NOW(),
-    modify_dttm TIMESTAMPTZ,
+    create_dttm TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    modify_dttm TIMESTAMP WITH TIME ZONE,
     status status DEFAULT 'created'::status
 );
 
@@ -29,20 +29,20 @@ COMMENT ON COLUMN request.activity_id IS 'Ид. сферы';
 COMMENT ON COLUMN request.title IS 'Заголовок';
 COMMENT ON COLUMN request.description IS 'Описание';
 COMMENT ON COLUMN request.country_id IS 'Ид. страны';
-COMMENT ON COLUMN request. city_id IS 'Ид. города';
-COMMENT ON COLUMN request. create_dttm IS 'Время создания';
-COMMENT ON COLUMN request. modify_dttm IS 'Время последнего изменения';
-COMMENT ON COLUMN request. status IS 'Статус';
+COMMENT ON COLUMN request.city_id IS 'Ид. города';
+COMMENT ON COLUMN request.create_dttm IS 'Время создания';
+COMMENT ON COLUMN request.modify_dttm IS 'Время последнего изменения';
+COMMENT ON COLUMN request.status IS 'Статус';
 
 CREATE TABLE request_user (
-    request_user_id SERIAL PRIMARY KEY,
-    request_id INTEGER
+    request_user_id UUID PRIMARY KEY,
+    request_id UUID
         CONSTRAINT request_user_request_request_id_fkey
     	REFERENCES request (request_id)
     	ON UPDATE CASCADE ON DELETE RESTRICT,
-    user_id INTEGER NOT NULL,
-    create_dttm TIMESTAMPTZ DEFAULT NOW(),
-    modify_dttm TIMESTAMPTZ,
+    user_id UUID NOT NULL,
+    create_dttm TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    modify_dttm TIMESTAMP WITH TIME ZONE,
     status status DEFAULT 'created'::status
 );
 
@@ -53,6 +53,6 @@ COMMENT ON TABLE request_user IS 'Отклики на запросы';
 COMMENT ON COLUMN request_user.request_user_id IS 'Первичный ключ';
 COMMENT ON COLUMN request_user.request_id IS 'Ид. запроса';
 COMMENT ON COLUMN request_user.user_id IS 'Ид. пользователя, который ответил на запрос';
-COMMENT ON COLUMN request_user. create_dttm IS 'Время создания';
-COMMENT ON COLUMN request_user. modify_dttm IS 'Время последнего изменения';
-COMMENT ON COLUMN request_user. status IS 'Статус';
+COMMENT ON COLUMN request_user.create_dttm IS 'Время создания';
+COMMENT ON COLUMN request_user.modify_dttm IS 'Время последнего изменения';
+COMMENT ON COLUMN request_user.status IS 'Статус';
